86a8956ce88fc2573046af0a5535b9b5296ff932
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-06-28 14:02:33 +0530

    Collection Fram Work In Java

--- Collection/Collection_basic_Function_2.java
+++ Collection/Collection_basic_Function_2.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Collection_basic_Function_2 {
     public static void main(String[] args) {
         // Create a List of integers
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
+        list.add(99);
+        list.add(33);
+        list.add(111);
         System.out.println("Element at index 1: " + list.get(1));
         System.out.println("Before Set List: " + list);
 
@@ -20,7 +21,13 @@ public class Collection_basic_Function_2 {
             System.out.print(obj + " ");
         }
 
+        list.add(22);
+        list.add(33);
         // Contains
-        System.out.println("List contains 20: " + list.contains(20));
+        System.out.println("List contains 33: " + list.contains(33));
+
+        // Sort an ArrayList
+        Collections.sort(list);
+        System.out.println("Sorted List: " + list);
     }
 }
