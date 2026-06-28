82e4616f24cc939a0168fe1ac0c62aa6cd198ddb
hnhpatel66-coder <hnhpatel66@gmail.com>
2026-06-28 14:05:18 +0530

    polymorphism

--- OOP/Polymorphism_method_overloading.java
+++ OOP/Polymorphism_method_overloading.java
@@ -16,6 +16,7 @@ class calculator {
 
 public class Polymorphism_method_overloading {
     public static void main(String[] args) {
+    //   compile time polymorephism (early binding)
     //     System.out.println("Method Overloading");
     //     System.out.println("Method Overloading is a feature that allows a class to have more than one method having the same name, if their parameter lists are different.");
     //     System.out.println("It is related to compile time polymorphism.");
